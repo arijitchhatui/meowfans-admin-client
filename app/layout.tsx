3fc64fe460e8f58fd035283238865c89898c787b
ARIJIT <arijitchhatui.me@gmail.com>
2025-09-27 22:24:31 +0530

    fix: update query hook to mutation hook (#6)

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,14 @@ import { AppBottomNav } from '@/components/AppBottomNav';
 import { AppSidebar } from '@/components/AppSideBar';
 import { SidebarProvider } from '@/components/ui/sidebar';
 import { fetchRequest } from '@/hooks/api/useAPI';
+import { CreatorContextWrapper } from '@/hooks/context/CreatorContextWrapper';
 import { AppConfig } from '@/lib/app.config';
 import { authCookieKey, FetchMethods, UserRoles } from '@/lib/constants';
 import { cn } from '@/lib/utils';
+import { getClient } from '@/packages/gql/ApolloClient';
 import { ApolloWrapper } from '@/packages/gql/ApolloWrapper';
+import { GET_CREATOR_PROFILE_QUERY } from '@/packages/gql/api/creatorAPI';
+import { GetCreatorProfileQuery } from '@/packages/gql/generated/graphql';
 import { configService } from '@/util/config';
 import { buildSafeUrl, decodeJwtToken } from '@/util/helpers';
 import { Theme } from '@radix-ui/themes';
@@ -66,7 +70,13 @@ const verifyAccessToken = async (token: string) => {
   return data;
 };
 
-const handleValidate = async () => {
+const getUser = async () => {
+  const client = await getClient();
+  const { data } = await client.query({ query: GET_CREATOR_PROFILE_QUERY });
+  return data;
+};
+
+const handleValidateAuth = async () => {
   const cookiesList = await cookies();
   const accessToken = cookiesList.get(authCookieKey)?.value;
 
@@ -78,14 +88,15 @@ const handleValidate = async () => {
 
   if (!accessToken) return redirect(buildSafeUrl({ host: configService.NEXT_PUBLIC_AUTH_URL }));
   try {
-    await verifyAccessToken(accessToken);
+    const response = await verifyAccessToken(accessToken);
+    if (response !== null) return await getUser();
   } catch {
     return redirect(buildSafeUrl({ host: configService.NEXT_PUBLIC_AUTH_URL }));
   }
 };
 
 export default async function RootLayout({ children }: Props) {
-  await handleValidate();
+  const user = await handleValidateAuth();
 
   return (
     <html lang="en" suppressHydrationWarning>
@@ -112,7 +123,9 @@ export default async function RootLayout({ children }: Props) {
             >
               <SidebarProvider>
                 <AppSidebar />
-                <main className="w-full">{children}</main>
+                <CreatorContextWrapper creator={user as GetCreatorProfileQuery}>
+                  <main className="w-full">{children}</main>
+                </CreatorContextWrapper>
               </SidebarProvider>
               <AppBottomNav />
             </ThemeProvider>
